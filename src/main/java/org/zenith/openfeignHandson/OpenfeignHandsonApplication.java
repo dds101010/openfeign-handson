6a775a7a5009412111c4172757dfde2380ee802d
Darshit Shah <ddshah24595@gmail.com>
2022-11-20 11:23:17 +0530

    Initial commit

---
+++ src/main/java/org/zenith/openfeignHandson/OpenfeignHandsonApplication.java
@@ -0,0 +1,13 @@
+package org.zenith.openfeignHandson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenfeignHandsonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpenfeignHandsonApplication.class, args);
+    }
+
+}
